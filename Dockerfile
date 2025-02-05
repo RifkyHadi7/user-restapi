492a2f6c542d6ebca80a28f0f5c9a0cca456d1b6
RifkyHadi7 <rifkyhadi7@gmail.com>
2025-02-06 01:01:12 +0700

    Development: Setup Dockerfile and Docker Compose

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+# Use Node.js image as the base image
+FROM node:20-alpine
+
+# Set the working directory inside the container
+WORKDIR /usr/src/app
+
+# Install postgresql-client to use pg_isready
+RUN apk add --no-cache postgresql-client
+
+# Copy package.json and package-lock.json files into the container
+COPY package*.json ./
+
+# Install the required dependencies
+RUN npm install
+
+# Copy all application source files into the container
+COPY . .
+
+# Install additional required dependencies
+RUN npm install @nestjs/passport passport passport-jwt \
+  @nestjs/jwt jsonwebtoken bcryptjs class-validator class-transformer
+
+# Build the NestJS application
+RUN npm run build
+
+# Expose the port used by the application
+EXPOSE 3000
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,14 +2,15 @@ import { Body, Controller, Get, Post, UseGuards, Req, HttpStatus, Res } from '@n
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth.guard';
 import { successResponse } from '../config/responses.config';
+import { LoginDto } from 'src/dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body() body: { identifier: string; password: string }, @Res() res) {
-    return this.authService.validateUser(body.identifier, body.password, res);
+  async login(@Body() loginDto : LoginDto, @Res() res) {
+    return this.authService.validateUser(loginDto.identifier, loginDto.password, res);
   }
 
   @Get('profile')
